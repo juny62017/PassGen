a7c360c89cc52ef9359a29d444443936812f36cc
juny62017 <juny62017@gmail.com>
2026-05-12 15:48:19 +0530

    Added password generation feature

--- script.js
+++ script.js
@@ -4,4 +4,99 @@ let passwordOutput =
 let generateBtn =
     document.querySelector(".generate-btn");
 
-console.log("PassGen Started");
+let lengthSlider =
+    document.getElementById("lengthSlider");
+
+let lengthText =
+    document.querySelector(".length-text");
+
+let uppercaseCheck =
+    document.getElementById("uppercaseCheck");
+
+let lowercaseCheck =
+    document.getElementById("lowercaseCheck");
+
+let numbersCheck =
+    document.getElementById("numbersCheck");
+
+let symbolsCheck =
+    document.getElementById("symbolsCheck");
+
+let uppercaseLetters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let lowercaseLetters =
+    "abcdefghijklmnopqrstuvwxyz";
+
+let numberCharacters =
+    "0123456789";
+
+let symbolCharacters =
+    "!@#$%^&*()_+?><";
+
+lengthSlider.oninput = function () {
+
+    lengthText.innerText =
+        "Length: " + lengthSlider.value;
+
+};
+
+generateBtn.onclick = function () {
+
+    let finalCharacters = "";
+
+    let generatedPassword = "";
+
+    if (uppercaseCheck.checked === true) {
+
+        finalCharacters += uppercaseLetters;
+
+    }
+
+    if (lowercaseCheck.checked === true) {
+
+        finalCharacters += lowercaseLetters;
+
+    }
+
+    if (numbersCheck.checked === true) {
+
+        finalCharacters += numberCharacters;
+
+    }
+
+    if (symbolsCheck.checked === true) {
+
+        finalCharacters += symbolCharacters;
+
+    }
+
+    if (finalCharacters === "") {
+
+        passwordOutput.value =
+            "Select at least one option";
+
+        return;
+    }
+
+    for (
+        let i = 0;
+        i < lengthSlider.value;
+        i++
+    ) {
+
+        let randomIndex =
+            Math.floor(
+                Math.random() *
+                finalCharacters.length
+            );
+
+        generatedPassword +=
+            finalCharacters[randomIndex];
+
+    }
+
+    passwordOutput.value =
+        generatedPassword;
+
+};
